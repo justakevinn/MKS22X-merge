4f0f5afa441e6b87d6b3c801bb07bdd1f9891377
Kevin <kzong@stuy.edu>
2019-03-18 09:42:05 -0400

    Drafted mergesort

--- Merge.java
+++ Merge.java
@@ -11,14 +11,27 @@ mergesort(data,lo,hi):
 public class Merge{
 
   public static void mergesort(int[]data){
-    return mergesortH(data, 0, data.length);
+    int[] temp = new int[data.length];
+    for (int i = 0; i < data.length; i++){
+      temp[i] = data[i];
+    }
+    return mergesortH(data, temp, 0, data.length-1);
   }
 
-  public static void mergesortH(int data, int lo, int hi){
-    if (lo >= hi){
-      return;
+  public static void mergesortH(int[] data, int[] temp; int lo, int hi){
+    int half = (hi + lo) / 2;
+    if (lo < hi){
+      int[] left = new int [half-lo+1];
+      int[] right = new int [hi - half];
+      for (int i = 0; i < left.length; i++) {
+        left[i] = data[lo+i];
+      }
+      for (int i = 0; i < right.length; i++) {
+        right[i] = data[half+i+1];
+      }
+      mergesortH(left, 0, left.length-1);
+      mergesortH(right, 0, right.length-1);
     }
-    int half = hi / 2;
   }
 
   private static void swap(int[] data, int x, int y){
